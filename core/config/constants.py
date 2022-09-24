4a68ebcbaa8d1d1da716043eca2811c763feeb92
noiz.e <10823729+noize-e@users.noreply.github.com>
2022-09-23 19:53:41 -0500

    Update constants.py

--- core/config/constants.py
+++ core/config/constants.py
@@ -1,23 +1,7 @@
-# whiterose
-TIMEZONES = {
-    "MX": "Mexico/General"
-}
-
-TRACE_STDOUT = True
-
-# Webhhok
-HOOK_POST = True
-HOOK_URL = "https://hooks.slack.com/services/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"
-HOOK_OK = "Operation Successful"
-HOOK_ERR = "System Failure"
-
-# logs
-LOG_LEVEL = "info"
-LOG_URL = "https://console.aws.amazon.com/cloudwatch/home?region=us-east-1#logs:"
-
 # HTTP Request
 HTTP_METHODS = ['GET', 'POST', 'PUT', 'DELETE', 'HEAD']
 HTTP_CONTENT_TYPE = 'application/json'
+# C.O.R.S
 CORS_HEADERS = ['Content-Type',
                 'X-Amz-Date',
                 'Authorization',
